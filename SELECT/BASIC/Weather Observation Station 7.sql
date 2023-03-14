c1da8db0cb509b7e97a1957db7c0b34d1b30c071
Dwi Rachmat Putra <72707099+dwirput@users.noreply.github.com>
2023-03-14 23:54:14 +0700

    Create Weather Observation Station 7.sql

---
+++ SELECT/BASIC/Weather Observation Station 7.sql
@@ -0,0 +1,22 @@
+/*Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| CITY        | VARCHAR2(21) |
+| STATE       | VARCHAR2(2)  |
+| LAT_N       | NUMBER       |
+| LAT_W       | NUMBER       |
++-------------+--------------+
+*/
+
+select distinct city
+from station
+where city like '%a' || city like '%i' || city like '%u' || city like '%e' || city like '%o'
+
+-- The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.
+-- Like '%a' mean Finds any values that end with "a"
